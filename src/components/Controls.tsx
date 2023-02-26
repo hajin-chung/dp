2c98f79ea4743b3e4897209dbb79dd083e6e826b
hajin-chung <hajinchung1@gmail.com>
2023-02-27 03:21:24 +0900

    feat: dashboard button on controls

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -2,7 +2,7 @@ import Sun from "@/static/sun.svg";
 import Moon from "@/static/Moon.svg";
 import { useStore } from "@nanostores/solid";
 import { theme } from "@/utils/theme";
-import { createEffect } from "solid-js";
+import { Component, createEffect } from "solid-js";
 
 export const Controls = () => {
   const $theme = useStore(theme);
@@ -16,7 +16,13 @@ export const Controls = () => {
   });
 
   return (
-    <div class="fixed bottom-2 right-2">
+    <div class="fixed bottom-2 right-2 flex flex-col gap-2">
+      <button
+        class="relative h-9 w-9 rounded-lg bg-slate-900 bg-opacity-10 p-1 dark:bg-white dark:bg-opacity-10"
+        onClick={() => window.location.replace("/dashboard")}
+      >
+        D
+      </button>
       <button
         class="relative h-9 w-9 rounded-lg bg-slate-900 bg-opacity-10 p-1 dark:bg-white dark:bg-opacity-10"
         onClick={() => {
